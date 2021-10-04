d6164cd9cf1bd4a76e29d96e884fe20dfc65e694
Tait Brown <taitbrown@gmail.com>
2021-10-04 21:49:42 +1100

    ADDED: missed file

---
+++ src/getCompanyFromEmail.test.js
@@ -0,0 +1,7 @@
+const getCompanyFromEmail = require('./getCompanyFromEmail');
+
+test('Company detection', () => {
+    expect(getCompanyFromEmail('hello@gmail.com')).toBe(null)
+    expect(getCompanyFromEmail('hello@company.com')).toBe('company.com')
+    expect(getCompanyFromEmail('hello@acloud.guru')).toBe('acloud.guru')
+})
