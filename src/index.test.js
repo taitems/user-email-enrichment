b98f325dc9f5ee3aadc8bc5b76de18ca6f881590
Tait Brown <taitbrown@gmail.com>
2021-10-08 08:39:58 +1100

    committing deliberately broken test for CI

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
 const enrich = require('./index');
 
 test('throws on empty', () => {
-    return expect(enrich()).rejects.toMatch('error');
+    return expect(enrich('broken@company.com')).rejects.toMatch('error');
 })
