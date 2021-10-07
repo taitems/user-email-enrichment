fe7d568bfc9d107ac0533c5eb5159cb07bbd3c40
Tait Brown <taitbrown@gmail.com>
2021-10-07 21:16:52 +1100

    ADDED: CRA first commit

---
+++ demo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { DemoInput } from './components/DemoInput';
+
+function App() {
+  return (
+    <div className="App">
+      <DemoInput />
+    </div>
+  );
+}
+
+export default App;
