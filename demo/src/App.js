f1c1acf85bb4e7f17913127b0237f00dd34a936d
Tait Brown <taitbrown@gmail.com>
2021-10-15 17:52:35 +1100

    ADDED: new profile card design

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,30 +1,60 @@
 import './App.css';
 import React, { useState } from 'react';
 import enrich from 'enrich-email/dist/web/esm';
-import { Input } from '@chakra-ui/input';
-import { Button } from '@chakra-ui/button';
-import { FakeProfile } from './components/FakeProfile';
+import { Flex, Box, Text, Heading, Input, Button } from "@chakra-ui/react"
+// import { FakeProfile } from './components/FakeProfile';
+import { FakeForm } from './components/FakeForm';
+import { ProfileCard } from './components/ProfileCard';
 
 function App() {
 
   const [input, setInput] = useState('taitbrown@gmail.com');
+  const [outputMode, setOutputMode] = useState('profile');
   const [result, setResult] = useState(null);
 
   const handleClick = () => {
-      // console.log(input);
-      enrich(input).then(data => {
-        console.log(data)
-        setResult(data);
-      });
+    // console.log(input);
+    enrich(input).then(data => {
+      console.log(data)
+      setResult(data);
+    });
   }
   return (
-    <>
-      <div>
-          <Input type="email" value={input} onChange={e => { setInput(e.target.value)}} />
+    <Flex height="100vh" width="100%">
+      <Box width="40%">
+        <Text textTransform="uppercase" letterSpacing="0.035em" fontWeight="semibold" color="purple.700" fontSize="14">Demo</Text>
+        <Heading as="h1">Reverse Email Lookup</Heading>
+        <Text as="h2">A free, open source identity resolution alternative to tools like FullContact and Clearbit. </Text>
+        <div>
+          <Input type="email" value={input} onChange={e => { setInput(e.target.value) }} />
           <Button onClick={handleClick}>Submit</Button>
-      </div>
-      {result && <FakeProfile result={result} />}
-    </>
+        </div>
+      </Box>
+      <Flex width="60%" bg="gray.50" flexGrow="1" textAlign="center" flexDirection="column" alignItems="center">
+
+        <Box mt="8" mb="12">
+          <Button
+            onClick={() => setOutputMode('form')}
+            variant={outputMode === 'form' ? 'solid' : 'ghost'}
+          >
+            Register
+          </Button>
+          <Button onClick={() => setOutputMode('profile')}
+            variant={outputMode === 'profile' ? 'solid' : 'ghost'}>Profile</Button>
+          <Button onClick={() => setOutputMode('raw')}
+            variant={outputMode === 'raw' ? 'solid' : 'ghost'}>Raw data</Button>
+        </Box>
+
+        {!result && (
+          <Text>Please enter an email address to begin.</Text>
+        )}
+
+        {result && outputMode === 'form' && <FakeForm result={result} />}
+        {/* {result && outputMode === 'profile' && <FakeProfile result={result} />} */}
+        {result && outputMode === 'profile' && <ProfileCard result={result} />}
+        {result && outputMode === 'raw' && <pre>{JSON.stringify(result, null, 2)}</pre>}
+      </Flex>
+    </Flex>
   )
 }
 
---
+++ demo/src/components/ProfileCard.jsx
@@ -0,0 +1,48 @@
+import { Avatar, Img } from '@chakra-ui/react';
+import Icon from '@chakra-ui/icon';
+import { Box, Flex, Heading } from '@chakra-ui/layout';
+import { LocationMarkerIcon, OfficeBuildingIcon, GlobeIcon } from '@heroicons/react/solid'
+import React from 'react'
+
+export const ProfileCard = ({result}) => {
+    const { guess } = result;
+
+    return (
+        <Box
+            textAlign="left"
+            shadow="lg"
+            borderRadius="md"
+            p="2"
+        >
+            <Img src="banner.jpg" width="385" height="108" alt="" borderRadius="4" mb="-8" />
+            <Box ml="3">
+            <Box borderRadius="4px" borderWidth="5px" borderStyle="solid" borderColor="white" display="inline-block" >
+                <Avatar src={guess.avatar_url} name={guess.name || guess.displayName}  borderRadius="none" size="lg"/>
+            </Box>
+            <Heading as="h2" fontSize="3xl">
+                {guess.name}
+            </Heading>
+            {guess.displayName && <Heading as="h3" fontSize="lg" color="gray.500" my={2}>@{guess.displayName}</Heading>}
+            <p>
+                {guess.bio}
+            </p>
+            {guess.location && (
+                <Flex alignItems="center" color="gray.600">
+                    <Icon color="gray.400" as={LocationMarkerIcon} w={4} h={4} />{guess.location}
+                </Flex>
+            )}
+            {guess.company && (
+                <Flex alignItems="center" color="gray.600">
+                    <Icon color="gray.400" as={OfficeBuildingIcon} w={4} h={4} />{guess.company}
+                </Flex>
+            )}
+            {guess.website && (
+                <Flex alignItems="center" color="gray.600">
+                    <Icon color="gray.400" as={GlobeIcon} w={4} h={4} />{guess.website}
+                </Flex>
+            )}
+            </Box>
+
+        </Box>
+    )
+}
