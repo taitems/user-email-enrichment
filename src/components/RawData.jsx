90f1b23ccd21ef025e6526f614adafa26296dbce
Tait Brown <taitbrown@gmail.com>
2021-10-18 21:34:22 +1100

    ADDED: pretty json output

--- src/components/RawData.jsx
+++ src/components/RawData.jsx
@@ -1,12 +1,15 @@
 import { Box } from '@chakra-ui/layout'
+import { prettyPrintJson } from 'pretty-print-json';
+import 'pretty-print-json/dist/pretty-print-json.css';
 import React from 'react'
 
 export const RawData = ({result}) => {
+    const html = {
+        __html: prettyPrintJson.toHtml(result)
+    };
     return (
-        <Box textAlign="left">
-        <pre>
-            {result && JSON.stringify(result, null, 2)}
-        </pre>
+        <Box textAlign="left" fontSize={13} lineHeight={1.75}>
+        <pre dangerouslySetInnerHTML={html} />
         </Box>
     )
 }
