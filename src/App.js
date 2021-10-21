699369f7b07a5b2b084837824eda066a5709e835
Tait Brown <taitbrown@gmail.com>
2021-10-21 22:14:17 +1100

    ADDED: basic responsive view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import enrich from 'enrich-email/dist/web/esm';
-import { Flex, Box, Text, Heading, Input, InputGroup, InputRightElement, Button,Spinner } from '@chakra-ui/react';
+import { Flex, Box, Text, Heading, Input, InputGroup, InputRightElement, Button,Spinner, Link } from '@chakra-ui/react';
 import { ProfileCard } from './components/ProfileCard';
 import { RawData } from './components/RawData';
 import { RegisterCard } from './components/RegisterCard';
@@ -32,30 +32,30 @@ function App() {
 	};
 
 	return (
-		<Flex minHeight="100vh" height="100%" width="100%">
-			<Flex width="40%" justifyContent="center" flexDirection="column">
+		<Flex minHeight="100vh" height="100%" width="100%" flexDirection={['column', null, 'row']}>
+			<Flex width={['100%',null,'40%']} py={[20,16, 0]} textAlign={['center', null, 'left']} height={['auto',null,'100vh']} justifyContent="center" flexDirection="column">
 				<Box px="8">
 					<Text textTransform="uppercase" letterSpacing="0.035em" fontWeight="semibold" color="purple.700" fontSize="14">Demo</Text>
 					<Heading as="h1">Reverse Email Lookup</Heading>
 					<Text as="h2" width="66%" minWidth="300px">A free, open source identity resolution alternative to tools like FullContact and Clearbit. </Text>
-					<div>
-						<InputGroup size="md" maxWidth="400px">
-							<Input
-								pr="4.5rem"
-								type="email"
-								placeholder="Enter an email address"
-								value={input}
-								onChange={e => { setInput(e.target.value); }} 
-							/>
-							<InputRightElement width="4.5rem">
-								<Button onClick={handleClick}>Submit</Button>
-							</InputRightElement>
-						</InputGroup>
-					</div>
-          Or try an <Text color="blue.700" onClick={() => { setInput(EXAMPLE1); search(EXAMPLE1); }}>example search</Text>
+					<InputGroup size="md" mt="4" maxWidth="400px">
+						<Input
+							pr="4.5rem"
+							type="email"
+							placeholder="Enter an email address"
+							value={input}
+							onChange={e => { setInput(e.target.value); }} 
+						/>
+						<InputRightElement width="4.5rem">
+							<Button onClick={handleClick}>Submit</Button>
+						</InputRightElement>
+					</InputGroup>
+					<Text fontSize="sm" mt="2">
+          Or try an <Link color="blue.700" onClick={() => { setInput(EXAMPLE1); search(EXAMPLE1); }}>example search</Link>
+					</Text>
 				</Box>
 			</Flex>
-			<Flex width="60%" bg="gray.50" flexGrow="1" textAlign="center" flexDirection="column" alignItems="center">
+			<Flex width={['100%',null,'60%']} bg="gray.50" flexGrow="1" textAlign="center" flexDirection="column" alignItems="center">
 
 				<Box mt="8" mb="12">
 					<Button
@@ -70,22 +70,20 @@ function App() {
 						variant={outputMode === 'raw' ? 'solid' : 'ghost'}>Raw data</Button>
 				</Box>
 
-				<Box position="relative">
+				{!result && (
+					<Text>Please enter an email address to begin.</Text>
+				)}
 
-					{!result && (
-						<Text>Please enter an email address to begin.</Text>
-					)}
+				{!loading && <Box position="fixed" right="20px" top="20px" marginLeft="-25px" zIndex="2">
+					<Flex width="50px" height="50px" borderRadius="md" background="rgba(0,0,0,0.5)" alignItems="center" justifyContent="center">
+						<Spinner color="white" />
+					</Flex>
+				</Box>}
 
-					{loading && <Box position="absolute" left="50%" top="100px" marginLeft="-10px">
-						<Spinner />
-					</Box>}
-
-					<Box opacity={loading ? .5 : 1}>
-						{result && outputMode === 'form' && <RegisterCard result={result} />}
-						{result && outputMode === 'profile' && <ProfileCard result={result} />}
-						{result && outputMode === 'raw' && <RawData result={result} />}
-					</Box>
-
+				<Box opacity={loading ? .5 : 1}>
+					{result && outputMode === 'form' && <RegisterCard result={result} />}
+					{result && outputMode === 'profile' && <ProfileCard result={result} />}
+					{result && outputMode === 'raw' && <RawData result={result} />}
 				</Box>
 
 			</Flex>
